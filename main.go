1a777decf59591019b1139ec8414e6ee19dc4c29
Your Name <you@example.com>
2022-04-09 12:24:23 +1200

    initial commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+type model struct {
+	IP string
+}
+
+func create() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	// fmt.Println(localAddr.IP)
+
+	return localAddr.IP
+
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+
+	var mdl model
+
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	mdl.IP = create().String()
+	fmt.Println(mdl.IP)
+
+	rspByt, err := json.Marshal(mdl)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf("error %v", err)))
+	}
+
+	w.Write(rspByt)
+	fmt.Println(mdl.IP)
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(
+		middleware.Logger,
+		middleware.StripSlashes,
+		cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300,
+			// Debug:            true,
+		}),
+	)
+
+	r.Get("/ip", get)
+
+	http.ListenAndServe(":8080", r)
+}
