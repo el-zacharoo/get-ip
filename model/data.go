b0c0e4b83f29d05d754a080508cdd803f1928990
Your Name <you@example.com>
2022-04-12 13:33:07 +1200

    iop added to field

--- model/data.go
+++ model/data.go
@@ -10,4 +10,5 @@ type Geolocation struct {
 	Country     string    `json:"country"`
 	CountryCode string    `json:"country_code"`
 	Date        time.Time `json:"date"`
+	IPAddress   string    `json:"ipAddress"`
 }
